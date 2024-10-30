6cd6488fb7a872dd13cad826297cd7ef4fc4c66b
jt3951 <jt3951@O321-25L526193>
2024-10-30 11:19:01 +0000

    Add project files.

---
+++ TourPlan_SLL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TourPlan_SLL;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SinglyLinkedList tour = new SinglyLinkedList();
+        
+        tour.insertLast("London");
+        tour.insertLast("Oxford");
+        tour.insertLast("Birmingham");
+        tour.insertLast("Manchester");        
+        tour.display();
+
+        tour.removeCity("Birmingham");
+        tour.display();        
+        tour.insertLast("York");
+        tour.display();
+
+
+    }
+}
---
+++ TourPlan_SLL/SinglyLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlan_SLL;
+
+namespace TourPlan_SLL
+{
+    internal class SinglyLinkedList
+    {
+        public Node head;
+        public SinglyLinkedList()
+        {
+            head = null;
+        }        
+        public void insertLast(string city) 
+        {
+            // Write Your code here to insert the city (given in parameter) at the end of the list
+        }
+       
+
+        public void removeCity(string city)
+        {
+            // Write your code here to remove the city given in parameter
+        }
+        public void display()
+        {
+            Node pointer = head;
+            if (pointer == null)
+            {
+                Console.WriteLine("List empty \n");
+                return;
+            }
+
+            while (pointer != null)
+            {
+                Console.Write(pointer.data + "----> ");
+                pointer = pointer.next;
+            }
+            Console.WriteLine("Null\n");
+        }
+    }
+}
